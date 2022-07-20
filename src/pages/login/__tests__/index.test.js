40a7af72747685a6e27c816e43ff4967640aaa92
Luana <luanaff-2008@hotmail.com>
2022-07-19 22:40:32 -0300

    fix: broken test

--- src/pages/login/__tests__/index.test.js
+++ src/pages/login/__tests__/index.test.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 import LoginPage from '../index';
 
 describe('Login Page tests', () => {
   it('find adicionar button', () => {
-    render(<LoginPage />);
-    expect(screen.getByText('adicionar')).toBeTruthy();
+    const history = createMemoryHistory();
+    render(
+      <Router location={history.location} navigator={history}>
+        <LoginPage />
+      </Router>
+    );
+    expect(screen.getByText('Entrar')).toBeTruthy();
   });
 });
